62837e7214714086f2217ffaf5a721ec7534f40d
mikestone <stonemj@gmail.com>
2011-08-25 10:10:49 -0700

    Initial commit of apache analyze

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apacheanalyze::Application.config.secret_token = 'faf6aa980d641daa3bde725acc14149a8330c43cd6979332e18dc72931bbe4a29802846aadbbe9d5eeaf66a048f08ce162d0b6997fcc49ccdde22ad778e87d6e'
