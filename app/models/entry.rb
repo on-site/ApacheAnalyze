8f51fb442db14d4033645da9d3c495f4476de4e0
mikestone <stonemj@gmail.com>
2011-08-25 17:17:22 -0700

    Parsing seems to be working now....

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,23 @@
 class Entry < ActiveRecord::Base
   belongs_to :source
+  VALID_GROUPS = [:client_ip, :access_time, :duration, :http_request, :status_code, :referrer, :user_agent].freeze
+
+  def parse!(regex, groups)
+    return if parsed
+
+    groups.each do |group|
+      next if group.blank?
+      raise "Invalid group #{group}" unless VALID_GROUPS.include? group.to_sym
+    end
+
+    return unless original =~ regex
+
+    groups.each_with_index do |group, index|
+      next if group.blank?
+      send "#{group}=", Regexp.last_match(index + 1)
+    end
+
+    self.parsed = true
+    save!
+  end
 end
