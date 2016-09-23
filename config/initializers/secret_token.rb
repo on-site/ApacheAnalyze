611660fee85ce9fc024e25e8a4715453fcc29820
Mike Virata-Stone <mjstone@on-site.com>
2016-09-23 14:08:42 -0700

    Upgrade to Rails 3.2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Apacheanalyze::Application.config.secret_token = '5e155f76b9905bb481d54b92a461563639580dbba103579862ed70bbe1233e2eaecda732f7c63a2131459d54e1188822c8183311ee383525a5d25a852236fbed'
+Apacheanalyze::Application.config.secret_token = 'b0d329c125758e7ddcae55bea8a916b4c9f005e9055d3eba83ea46849432d67edf7c8d6b031cc6297e15ef210429268a05a93b0bd471193dd970fdba3894cae1'
