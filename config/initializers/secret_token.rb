81d7cce474f6e527bd01c69b3a4648aa467b2058
Mike Virata-Stone <mjstone@on-site.com>
2016-09-23 14:11:07 -0700

    Upgrade to Rails 4.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Apacheanalyze::Application.config.secret_token = 'b0d329c125758e7ddcae55bea8a916b4c9f005e9055d3eba83ea46849432d67edf7c8d6b031cc6297e15ef210429268a05a93b0bd471193dd970fdba3894cae1'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apacheanalyze::Application.config.secret_key_base = '9e162a3c4ceb6c23d3fe63f1c46ba1c48a44ed6bb447a1dd2623b680e2bfff2727f4dcc735b5866560bf10fb9bbd040998c086701ff2c96a90767a64914bacbd'
