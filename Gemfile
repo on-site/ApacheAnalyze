aa71d89b3b19c50342d989168a8f25ffaf4dc7f1
Mike Virata-Stone <mjstone@on-site.com>
2016-09-23 14:13:05 -0700

    Upgrade to Rails 4.1

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "http://rubygems.org"
 
-gem "rails", "~> 4.0.0"
+gem "rails", "~> 4.1.0"
 gem "script_helpers"
 
 # Bundle edge Rails instead:
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Apacheanalyze::Application.routes.draw do
+Rails.application.routes.draw do
   get "analyze" => "analyze#index"
   post "analyze" => "analyze#load"
 
